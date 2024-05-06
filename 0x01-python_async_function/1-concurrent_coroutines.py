172b77ecaf73a0cd246ff18f6e175c631bdbad28
Richard <rcezea007@gmail.com>
2024-05-06 16:51:06 +0100

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,7 @@ from typing import List, Tuple
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Run multiple coroutines in one module"""
     arr: List[float] = []
     for _ in range(n):
