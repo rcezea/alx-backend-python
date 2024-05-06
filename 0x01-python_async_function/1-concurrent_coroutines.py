549d2e0e47bf3f25063987e29d199ae47682e6f5
Richard <rcezea007@gmail.com>
2024-05-06 22:45:05 +0100

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,6 +7,10 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Return the list of all the delays (float values)"""
-    coroutines: List[float] = [wait_random(max_delay) for _ in range(n)]
-    return sorted(await asyncio.gather(*coroutines))
+    """return the list of all the delays (float values)"""
+    arr: List[float] = []
+    for _ in range(n):
+        result = (await asyncio.gather(wait_random(max_delay)))[0]
+        if isinstance(result, float):
+            arr.append(result)
+    return sorted(arr)
