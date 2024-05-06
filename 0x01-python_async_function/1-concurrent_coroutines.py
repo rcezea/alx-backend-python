bb0c6c8693ab563ef0f16251a64ca41088363ef6
Richard <rcezea007@gmail.com>
2024-05-06 23:21:35 +0100

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,10 +9,17 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Return a list of all the delays (float values).
+
+    Args:
+        n (int): The number of coroutines to execute.
+        max_delay (int): The maximum delay for each coroutine.
+
+    Returns:
+        List[float]: Sorted list of delay values.
     """
-    arr = ()
-    for i in range(n):
-        arr += (await asyncio.gather(
-            *(wait_random(max_delay))
-        ))
+    arr: List[float] = []
+    for _ in range(n):
+        result = tuple(await asyncio.gather(wait_random(max_delay)))
+        if isinstance(result, float):
+            arr.append(result)
     return sorted(arr)
