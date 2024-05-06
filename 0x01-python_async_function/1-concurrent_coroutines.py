520281ac28e15b32a82566f4630252ad4882dcaf
Richard <rcezea007@gmail.com>
2024-05-06 20:00:49 +0100

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,5 +10,5 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """Run multiple coroutines in one module"""
     arr: List[float] = []
     for _ in range(n):
-        arr.append(*(await wait_random(max_delay)))
+        arr.append(await wait_random(max_delay))
     return sorted(arr)
