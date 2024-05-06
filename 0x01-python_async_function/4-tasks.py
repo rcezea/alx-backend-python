2393ddd59b9523e3f1bc1ee6c4db55490ff7ee97
Richard <rcezea007@gmail.com>
2024-05-06 21:04:07 +0100

    task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,7 +2,6 @@
 """Create a new function to alter wait_n"""
 from typing import List
 
-wait_random = __import__('0-basic_async_syntax').wait_random
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
@@ -10,6 +9,6 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Run multiple coroutines in one module"""
     arr: List[float] = []
     for _ in range(n):
-        arr.append(await wait_random(max_delay))
+        arr.append(await task_wait_random(max_delay))
     # task_wait_random(max_delay)
     return sorted(arr)
