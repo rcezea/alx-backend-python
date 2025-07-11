0b1c395fa1c16d328ee614a03d265e13fb48c844
Richard <rcezea007@gmail.com>
2025-07-11 23:05:22 +0100

    Memory-Efficient Aggregation with Generators

---
+++ python-generators-0x00/4-stream_ages.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+To use a generator to compute a memory-efficient
+aggregate function i.e average age for a large dataset
+"""
+
+seed = __import__('seed')
+connection = seed.connect_to_prodev()
+cursor = connection.cursor(dictionary=True)
+
+def stream_user_ages():
+    """ Get user ages """
+    query = 'SELECT age FROM user_data';
+    cursor.execute(query)
+    for row in cursor:
+        yield row
+
+
+def calculate_average():
+    """ Compute the average age of users. """
+    average = 0
+    num = 0
+    for num, age in enumerate(stream_user_ages(), start=1):
+        average += age["age"]
+    return average/num
