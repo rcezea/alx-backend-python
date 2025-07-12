18660857a0856461b1ad45040181445fa27a897e
Richard <rcezea007@gmail.com>
2025-07-12 16:58:55 +0100

    task 0

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -2,6 +2,7 @@
 
 import sqlite3
 import functools
+from datetime import datetime
 
 #### decorator to log SQL queries
 
@@ -10,7 +11,7 @@ def log_queries(f):
     """ log the SQL query before executing it. """
     functools.wraps(f)
     def wrapper_log_queries(*args, **kwargs):
-        print(kwargs)
+        print(f'[{ datetime.now()}] SQL Query: {kwargs.get("query")}')
         return f(*args, **kwargs)
     return wrapper_log_queries
 
