6a1751acd4f605a911b042a43b1990be32ea6534
Richard <rcezea007@gmail.com>
2025-07-12 18:27:39 +0100

    task 1

---
+++ python-decorators-0x01/1-with_db_connection.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+from sqlite3 import connect, Error
+import functools
+
+def with_db_connection(func):
+    """ your code goes here"""
+    @functools.wraps(func)
+    def wrapper_db_connection(*args, **kwargs):
+        # Open Database Connection
+        conn = None
+        try:
+            conn = connect('users.db')
+            return func(conn, *args, **kwargs)
+        except Error as e:
+            print(f"Database Connection Failed - {e}")
+        finally:
+            # Close Database Connection
+            if conn is not None:
+                conn.close()
+    return wrapper_db_connection
+
+@with_db_connection
+def get_user_by_id(conn, user_id):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,))
+    return cursor.fetchone()
+#### Fetch user by ID with automatic connection handling
+
+user = get_user_by_id(user_id=1)
+print(user)
