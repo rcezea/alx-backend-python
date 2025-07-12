c5fe6d69d364f253a36f8c66cd793120c2e8780c
Richard <rcezea007@gmail.com>
2025-07-12 20:15:43 +0100

    task 1

--- python-decorators-0x01/1-with_db_connection.py
+++ python-decorators-0x01/1-with_db_connection.py
@@ -19,13 +19,13 @@ def with_db_connection(func):
             if conn is not None:
                 conn.close()
     return wrapper_db_connection
+if __name__ == "__main__":
+    @with_db_connection
+    def get_user_by_id(conn, user_id):
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,))
+        return cursor.fetchone()
+    #### Fetch user by ID with automatic connection handling
 
-@with_db_connection
-def get_user_by_id(conn, user_id):
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,))
-    return cursor.fetchone()
-#### Fetch user by ID with automatic connection handling
-
-user = get_user_by_id(user_id=1)
-print(user)
+    user = get_user_by_id(user_id=1)
+    print(user)
