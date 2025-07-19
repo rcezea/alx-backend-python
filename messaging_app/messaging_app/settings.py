42542905e64474079aaab517d7e6c4c1440fdd6c
Richard <rcezea007@gmail.com>
2025-07-19 23:49:00 +0100

    Project set up

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -41,9 +41,14 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
+
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
+    ],
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
     ],
 }
 
