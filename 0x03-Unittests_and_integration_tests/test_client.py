8995ba9b6a01c88a3a73da1456e97c4c3cabe0c5
Richard <rcezea007@gmail.com>
2025-07-19 10:02:59 +0100

    removed the test on public_repos to work on testing _public_repos_url method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -22,16 +22,24 @@ class TestGithubOrgClient(unittest.TestCase):
         _ = GithubOrgClient(org).org
         mock.assert_called_with('https://api.github.com/orgs/{}'.format(org))
 
-    @patch('client.get_json')
-    def test_public_repos(self, mock_get):
-        """"""
-        obj = GithubOrgClient('google')
-        _ = obj.org
-        with patch.object(GithubOrgClient, '_public_repos_url', new_callable=PropertyMock) as mock:
-            mock.return_value = 'mocked_url'
-            _ = obj._public_repos_url
-            mock.assert_called_once()
-        mock_get.assert_called_once_with('https://api.github.com/orgs/google')
+    def test_public_repos_url(self):
+        """ method to unit-test GithubOrgClient._public_repos_url """
+        with patch.object(GithubOrgClient, 'org', new_callable=PropertyMock) as mock:
+            mock.return_value = {'repos_url': 'https://api.github.com/orgs/google/repos'}
+            obj = GithubOrgClient('google')
+            result = obj._public_repos_url
+            self.assertEqual(result, mock.return_value['repos_url'])
+
+    # @patch('client.get_json')
+    # def test_public_repos(self, mock_get):
+    #     """"""
+    #     obj = GithubOrgClient('google')
+    #     _ = obj.org
+    #     with patch.object(GithubOrgClient, '_public_repos_url', new_callable=PropertyMock) as mock:
+    #         mock.return_value = 'mocked_url'
+    #         _ = obj._public_repos_url
+    #         mock.assert_called_once()
+    #     mock_get.assert_called_once_with('https://api.github.com/orgs/google')
 
     @parameterized.expand([
         ({"license": {"key": "my_license"}}, "my_license", True),
