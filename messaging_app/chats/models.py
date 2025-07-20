ce1f03d1dd143b10481a5687efd0414a262feda3
Richard <rcezea007@gmail.com>
2025-07-20 02:18:44 +0100

    Define data Models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,38 @@
-from django.db import models
+from django.db.models import Model, UUIDField, CharField, DateTimeField, ForeignKey, CASCADE, TextField, TextChoices
+from django.utils import timezone
+
 
 # Create your models here.
+
+
+class RoleChoices(TextChoices):
+    GUEST = 'guest', 'Guest'
+    HOST = 'host', 'Host'
+    ADMIN = 'admin', 'Admin'
+
+class User(Model):
+    """ User Table """
+    user_id = UUIDField(primary_key=True, db_index=True)
+    first_name = CharField(max_length=256, null=False)
+    last_name = CharField(max_length=256, null=False)
+    email = CharField(max_length=256, null=False, unique=True)
+    password_hash = CharField(max_length=256, null=False)
+    phone_number = CharField(max_length=256, null=True)
+    role = CharField(max_length=10, choices=RoleChoices.choices, null=False)
+    created_at = DateTimeField(default=timezone.now)
+
+
+class Message(Model):
+    """ Message Table """
+    message_id = UUIDField(primary_key=True)
+    sender_id = ForeignKey(User, on_delete=CASCADE)
+    message_body = TextField(null=False)
+    sent_at = DateTimeField(default=timezone.now)
+
+
+class Conversation(Model):
+    """ Conversation Table """
+    conversation_id = UUIDField(primary_key=True, db_index=True)
+    participants_id = ForeignKey(User, on_delete=CASCADE)
+    created_at = DateTimeField(default=timezone.now)
+
