c47e6d728d648f68efd77041a29080e7d8e10211
Richard <rcezea007@gmail.com>
2025-07-30 00:30:17 +0100

    changed import style

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -3,17 +3,14 @@ Custom Django Permission Classes
 for User Authentication AND Authorization
 """
 
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
 
-class IsParticipantOfConversation(BasePermission):
+class IsParticipantOfConversation(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user in obj.participants.all()
 
 
-class IsSenderOfMessage(BasePermission):
+class IsSenderOfMessage(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.sender == request.user
-
-    # def has_permission(self, request, view):
-    #     return not bool(request.user and request.user.is_authenticated)
