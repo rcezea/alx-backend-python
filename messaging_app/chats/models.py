42dfcaa8a663d480c1ade1f6c524828f07e4e91b
Richard <rcezea007@gmail.com>
2025-07-30 00:22:28 +0100

    Added ConversationParticipant model to modify on_delte attribute of th JOIN table

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -7,14 +7,11 @@ from django.db.models import (
     TextChoices, ManyToManyField
 )
 from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
 
 
-# Create your models here.
-
-
-class User(Model):
+class User(AbstractUser):
     """ User Table """
-
     class RoleChoices(TextChoices):
         GUEST = 'guest', 'Guest'
         HOST = 'host', 'Host'
@@ -28,7 +25,17 @@ class User(Model):
     password_hash = CharField(max_length=256, null=False)
     phone_number = CharField(max_length=256, null=True)
     role = CharField(max_length=10, choices=RoleChoices.choices,
-                     null=False)
+                     null=False, default=RoleChoices.GUEST)
+    created_at = DateTimeField(default=timezone.now)
+
+
+class Conversation(Model):
+    """ Conversation Table """
+    conversation_id = UUIDField(primary_key=True, db_index=True,
+                                default=uuid.uuid4, editable=False)
+    participants = ManyToManyField(User, related_name='conversations',
+                                   through='ConversationParticipant',
+                                   blank=True)
     created_at = DateTimeField(default=timezone.now)
 
 
@@ -37,15 +44,20 @@ class Message(Model):
     message_id = UUIDField(primary_key=True, db_index=True,
                            default=uuid.uuid4, editable=False)
     sender = ForeignKey(User, on_delete=CASCADE)
-    conversation_id = ForeignKey('Conversation',
+    conversation_id = ForeignKey(Conversation,
                                  on_delete=CASCADE, related_name='messages')
     message_body = TextField(null=False)
     sent_at = DateTimeField(default=timezone.now)
 
 
-class Conversation(Model):
-    """ Conversation Table """
-    conversation_id = UUIDField(primary_key=True, db_index=True,
-                                default=uuid.uuid4, editable=False)
-    participants = ManyToManyField(User, related_name='conversations')
-    created_at = DateTimeField(default=timezone.now)
+class ConversationParticipant(Model):
+    conversation = ForeignKey(
+        Conversation, on_delete=CASCADE
+    )
+    user = ForeignKey(
+        User, on_delete=CASCADE
+    )
+
+    class Meta:
+        db_table = 'chats_conversation_participants'
+        unique_together = ('conversation', 'user')
