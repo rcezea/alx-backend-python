867d2d74c1ca07b35454d7e0412d9cab557427cb
Richard <rcezea007@gmail.com>
2025-07-30 00:40:54 +0100

    Enforce authentication for all API access

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -8,9 +8,17 @@ from rest_framework import permissions
 
 class IsParticipantOfConversation(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.user in obj.participants.all()
+        return (
+                request.user and
+                request.user.is_authenticated and
+                request.user in obj.participants.all()
+        )
 
 
 class IsSenderOfMessage(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.sender == request.user
+        return (
+                request.user and
+                request.user.is_authenticated and
+                obj.sender == request.user
+        )
