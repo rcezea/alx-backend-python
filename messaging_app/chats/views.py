52fc0058098cf146c195fcc00299eeaed367be36
Richard <rcezea007@gmail.com>
2025-07-30 00:25:11 +0100

    Ensured only participants of conversations have access to conversation and its messages

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+"""
+Module for DRF API Views
+"""
+
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from .models import Message, Conversation, User
+from .models import Message, Conversation, User, ConversationParticipant
 from .serializers import (MessageSerializer, UserSerializer,
                           ConversationSerializer)
 from django_filters import rest_framework as filters
+from chats.permissions import IsSenderOfMessage, IsParticipantOfConversation
 
 
 # Create your views here.
@@ -27,20 +31,65 @@ class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
 
+    def get_queryset(self):
+        return Message.objects.filter(
+            conversation_id__participants=self.request.user)
+
     def perform_create(self, serializer):
-        message = serializer.save()
-
+        message = serializer.save(sender=self.request.user)
         conversation = message.conversation_id
-
         if message.sender not in conversation.participants.all():
             conversation.participants.add(message.sender)
 
     def create(self, request, *args, **kwargs):
-        super().create(request, *args, **kwargs)
-        return Response({'status': 'Message created successfully'},
-                        status=status.HTTP_201_CREATED)
+        data = request.data.copy()
+        data['sender'] = str(request.user.pk)
+        # or `request.user.id`
+
+        # Check and Create New Conversation IF NOT EXISTS
+        """if not data.get('conversation_id'):
+            # print('Error due to parallax')
+            conversation = Conversation.objects.create()
+            data['conversation_id'] = conversation.conversation_id"""
+        # NOT REQUIRED SO COMMENT OUT
+
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def get_permissions(self):
+        if self.request.method in ['GET']:
+            return [IsSenderOfMessage()]
+        return super().get_permissions()
 
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
+    permission_classes = [IsParticipantOfConversation]
+
+    def get_queryset(self):
+        # Return only conversations where the user is a participant
+        return Conversation.objects.filter(participants=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+
+        participants = request.data.get("participants", [])
+        if isinstance(participants, str):
+            participants = request.data.getlist("participants")
+
+        if not participants or len(participants) < 2:
+            return Response(
+                {"error": "A conversation must have at least 2 participants."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        users = User.objects.filter(user_id__in=participants)
+        conversation = Conversation.objects.create()
+        for user in users:
+            (ConversationParticipant.objects
+             .create(conversation=conversation, user=user))
+        conversation.participants.set(users)
+
+        serializer = self.get_serializer(conversation)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
