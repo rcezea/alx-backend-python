869af6394a2a25ed0ba7eabb50a6841eeafd6d65
Richard <rcezea007@gmail.com>
2025-07-30 00:12:21 +0100

    Added routes for generating and refreshing session tokens

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from chats.auth import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("chats.urls")),
     path('api-auth/', include('rest_framework.urls')),  # <-- Required for checkers
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
