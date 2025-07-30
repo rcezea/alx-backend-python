ef0d48fbd50e5313edbbafad72389f2e068b0f37
Richard <rcezea007@gmail.com>
2025-07-30 21:09:28 +0100

    Pagination and Filtering

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -141,7 +141,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagePagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
 }
 
